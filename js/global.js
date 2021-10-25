dcd35f6d98bd92fcd0f1dd53cbfd16449f60b962
hanweiwu <8838261+hanweiwu@user.noreply.gitee.com>
2021-10-26 05:12:05 +0800

    first commit

---
+++ js/global.js
@@ -0,0 +1,2 @@
+var myurl = "http://wuhanwei.qicp.vip"
+var forwardUrl = "http://nbaapi.jumaojiang.top"
---
+++ js/my.js
@@ -0,0 +1,17 @@
+"\t\t\t\t\t\t<div class=\"preview\" >\n" +
+"\t\t\t\t\t\t\t<div class=\"preview_title\">检验员:</div>\n" +
+"\t\t\t\t\t\t\t<div class=\"preview_content\">08</div>\n" +
+"\t\t\t\t\t\t</div>\n" +
+"\t\t\t\t\t\t<div class=\"preview\" >\n" +
+"\t\t\t\t\t\t\t<div class=\"preview_title\">等级:</div>\n" +
+"\t\t\t\t\t\t\t<div class=\"preview_content\">合格品</div>\n" +
+"\t\t\t\t\t\t</div>\n" +
+"\t\t\t\t\t\t<p style=\"margin:0 0 0 10%; font-size: 0.7rem;\">(此条码仅供内部使用)</p>\n" +
+"\t\t\t\t\t\t<canvas class=\"canvascode\"></canvas>\n" +
+"\t\t\t\t\t\t<div class=\"barcode\" style=\"margin:0 0 0 10%;\">----------------</div>\n" +
+"\t\t\t\t\t\t\n" +
+"\t\t\t\t\t\t<div style=\"margin: 2px auto 6px auto; font-size: 1.5rem; text-align: center;\">\n" +
+"\t\t\t\t\t\t\t<div>零售价 : --------</div>\n" +
+"\t\t\t\t\t\t</div>\n" +
+"\t\t\t\t\t</div>\n" +
+"\t\t\t\t</div>"
